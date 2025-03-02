63e78e4548cd121fedab7ef53928d0cfd6237c8c
Misho1019 <mikailtorres99@gmail.com>
2025-03-02 03:11:01 +0200

    Close create user modal

--- client/src/components/UserList.jsx
+++ client/src/components/UserList.jsx
@@ -16,16 +16,20 @@ export default function UserList() {
             })
     }, [])
 
-    const addUserClickHandler = () => {
+    const createUserClickHandler = () => {
         setShowCreate(true)
         
+    }
+
+    const closeCreateUserClickHandler = () => {
+        setShowCreate(false)
     }
     return (
         <section className="card users-container">
             {/* <!-- Search bar component --> */}
             <Search />
 
-            {showCreate && <UserCreate />}
+            {showCreate && <UserCreate onClose={closeCreateUserClickHandler} />}
 
             {/* <!-- Table component --> */}
             <div className="table-wrapper">
@@ -149,7 +153,7 @@ export default function UserList() {
             </div>
 
             {/* <!-- New user button  --> */}
-            <button className="btn-add btn" onClick={addUserClickHandler}>Add new user</button>
+            <button className="btn-add btn" onClick={createUserClickHandler}>Add new user</button>
 
             {/* <!-- Pagination component  --> */}
             <Pagination />
